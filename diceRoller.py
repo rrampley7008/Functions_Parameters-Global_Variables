2237ec9c9d2bf5d2af1ca00727151aebc71d0834
RampleStiltSkin <rrampley7008@sohamcollege.org.uk>
2014-05-15 09:41:32 +0100

    correct die roll

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,9 @@
+
+
 #Coin flip program
 #Describe the purpose of this program here.
 
-inport random;time
+import random,time#import was spelt wrong and the semi-colon to a comma
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -11,25 +13,25 @@ s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+    print("rolling.....")
+    roll = random.randint(1,6)
+    return roll
 
-
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
+def show_dice(roll):
+    if roll == 1:
+        print(s1)
+    if roll == 2:
         print(s2)
-    else roll = 3
+    if roll == 3:
         print(s3)
-    else roll = 4
+    if roll == 4:
         print(s4)
-    else roll = 5
+    if roll == 5:
         print(s5)
-    else roll = 6
+    elif roll == 6:
         print(s6)
 
-roll
-time.sleep(1)
+roll = roll()
+time.sleep(3)
 show_dice(roll)
 
